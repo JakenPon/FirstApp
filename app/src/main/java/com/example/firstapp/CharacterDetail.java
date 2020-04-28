4658732d32b0aad3033e49a3c4730fe2e775b090
Sofiane SERKESTI <kingaldor78@gmail.com>
2020-04-28 21:08:29 +0200

    Ajout photo personnage

--- app/src/main/java/com/example/firstapp/CharacterDetail.java
+++ app/src/main/java/com/example/firstapp/CharacterDetail.java
@@ -6,17 +6,29 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 public class CharacterDetail extends AppCompatActivity {
     private TextView tv;
+    private ImageView iv;
+    private TextView status;
+    private TextView species;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_detail);
-        System.out.println("here");
+
         Character character = (Character) getIntent().getSerializableExtra("EXTRA_CHARACTER");
-        System.out.println(character.getName());
-        tv = findViewById(R.id.detail_name);
+        tv = findViewById(R.id.Name);
         tv.setText(character.getName());
+        iv = findViewById(R.id.Image_character);
+        Picasso.get()
+                .load(character.getImage())
+                .into(iv);
+        status =  findViewById(R.id.Status);
+        status.setText(character.getStatus());
+        species = findViewById(R.id.Species);
+        species.setText(character.getSpecies());
     }
 }
